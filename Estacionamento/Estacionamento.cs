932cbda3a91e487a85ab5e80deb4c44db536cca1
Jorgeluisreis <ctt.jorgeluis@gmail.com>
2023-01-10 23:01:56 -0300

    adição de um novo desafio

---
+++ Estacionamento/Estacionamento.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+
+namespace Estacionamento.Main
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            bool menu = true;
+            string addVeiculo, removeVeiculo;
+            int valorEntrada, valorHora, escolhaMenu, horasVeiculo;
+            Console.Clear();
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("     Sistema de Estacionamento      ");
+            Console.WriteLine("------------------------------------");
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(" Valor de entrada no estacionamento ");
+            Console.WriteLine("------------------------------------");
+            Console.Write("Resposta: ");
+            valorEntrada = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(" Valor cobrado por hora estacionado ");
+            Console.WriteLine("------------------------------------");
+            Console.Write("Resposta: ");
+            valorHora = int.Parse(Console.ReadLine());
+
+            List<string> Placas = new List<string>();
+
+
+        menu:
+            while (menu == true)
+            {
+                Console.Clear();
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("                Menu                ");
+                Console.WriteLine("------------------------------------");
+                Console.WriteLine("      1 - Cadastrar veiculo         ");
+                Console.WriteLine("       2 - Remover veiculo          ");
+                Console.WriteLine("       3 - Listar veiculos          ");
+                Console.WriteLine("          0 - Encerrar              ");
+                Console.WriteLine("------------------------------------");
+                Console.Write("    Opcao: ");
+                escolhaMenu = int.Parse(Console.ReadLine());
+
+                switch (escolhaMenu)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("         Cadastrar veiculo          ");
+                        Console.WriteLine("     Digite a placa do veiculo      ");
+                        Console.WriteLine("------------------------------------");
+                        Console.Write("    Resposta: ");
+                        addVeiculo = Convert.ToString(Console.ReadLine());
+                        Placas.Add(addVeiculo);
+                        Console.Clear();
+                        Console.WriteLine("    Veiculo cadastrao com sucesso!    ");
+                        Console.WriteLine("Aperte em qualquer botao para continuar");
+                        addVeiculo = null;
+                        Console.ReadKey();
+                        goto menu;
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("          Remover veiculo           ");
+                        Console.WriteLine("     Digite a placa do veiculo      ");
+                        Console.WriteLine("------------------------------------");
+                        foreach (string i in Placas)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.WriteLine("------------------------------------");
+                        Console.Write("    Resposta: ");
+
+                        removeVeiculo = Convert.ToString(Console.ReadLine());
+                        Placas.Remove(removeVeiculo);
+
+                        Console.Clear();
+                        Console.WriteLine("Aperte em qualquer botao para continuar");
+                        removeVeiculo = null;
+                        Console.Clear();
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("             Pagamento              ");
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("   Quantas horas o veiculo ficou ?  ");
+                        Console.WriteLine("------------------------------------");
+                        Console.Write("    Resposta: ");
+                        horasVeiculo = int.Parse(Console.ReadLine());
+                        Console.Clear();
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("             Pagamento              ");
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("     Veiculo removido com sucesso   ");
+                        Console.WriteLine($" e pagou R${(valorHora * horasVeiculo) + valorEntrada}. ");
+                        Console.WriteLine("------------------------------------");
+
+                        Console.ReadKey();
+                        goto menu;
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("         Lista de veiculos          ");
+                        Console.WriteLine("     Digite a placa do veiculo      ");
+                        Console.WriteLine("------------------------------------");
+                        foreach (string a in Placas)
+                        {
+                            Console.WriteLine(a);
+                        }
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("   Aperte em qualquer tecla para    ");
+                        Console.WriteLine("        retornar ao menu            ");
+                        Console.WriteLine("------------------------------------");
+                        Console.ReadKey();
+                        goto menu;
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Encerrando o programa...");
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Entrada inválida, aperte qualquer tecla");
+                        Console.ReadKey();
+                        goto menu;
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ Estacionamento/README.md
@@ -0,0 +1,4 @@
+## Desafio: Sistema de Estacionamento
+Desafio passado pela [DIO](https://dio.me) referente a criação de um sistema de estacionamento onde é cadastrado
+placas veiculares, controle de entrada e calculo de cota para retirar o veículo do mesmo, com calculo de horas
+estacionadas.
